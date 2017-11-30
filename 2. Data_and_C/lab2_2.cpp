126e9b7dc8105285fdfff9944d2b3110deeb1253
mjung1798 <mor2222@naver.com>
2017-11-30 12:47:09 +0900

    Add files via upload

---
+++ 2. Data_and_C/lab2_2.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void) 
+{
+	short a = 32765 + 100;
+	printf("a=%d\n", a);
+
+	return 0;
+}
---
+++ 2. Data_and_C/lab2_3.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void) 
+{
+	float grade;
+	scanf("%f", &grade);
+	printf("Your grade = %f\n", grade);
+	return 0;
+}
